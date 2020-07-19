79dab8706f22c233b69e6429a1eb2256cd3e5252
checrispUCF <54544426+checrispUCF@users.noreply.github.com>
2020-07-19 13:29:59 -0400

    First attempt to push anything to GitLab.  This app accepts user input and stores it dynamically

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [userName, set_userName] = useState("");
+  const [password, set_password] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={StyleSheet.container}>
+      <Text>Username: </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="e.g. jDoe"
+        onChangeText={(val) => set_userName(val)}
+      />
+
+      <Text>Password:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="e.g. password"
+        onChangeText={(val) => set_password(val)}
+      />
+
+      <Text>Stored Username: {userName}</Text>
+      <Text>Stored Password: {password}</Text>
     </View>
   );
 }
@@ -14,8 +30,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    boraderWidth: 1,
+    borderColor: "black",
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
