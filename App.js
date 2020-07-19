672a5068fa88c4464362a2dec692bb073010bb7d
checrispUCF <54544426+checrispUCF@users.noreply.github.com>
2020-07-19 14:49:29 -0400

    Figured out how to navigate using stack navigation.  Created a splash and login page with no styling

--- App.js
+++ App.js
@@ -1,44 +1,8 @@
 import React, { useState } from "react";
+import { AppLoading } from "expo";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import Navigator from "./web-build/routes/homestack";
 
 export default function App() {
-  const [userName, set_userName] = useState("");
-  const [password, set_password] = useState("");
-
-  return (
-    <View style={StyleSheet.container}>
-      <Text>Username: </Text>
-      <TextInput
-        style={styles.input}
-        placeholder="e.g. jDoe"
-        onChangeText={(val) => set_userName(val)}
-      />
-
-      <Text>Password:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="e.g. password"
-        onChangeText={(val) => set_password(val)}
-      />
-
-      <Text>Stored Username: {userName}</Text>
-      <Text>Stored Password: {password}</Text>
-    </View>
-  );
+  return <Navigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    boraderWidth: 1,
-    borderColor: "black",
-    padding: 8,
-    margin: 10,
-    width: 200,
-  },
-});
