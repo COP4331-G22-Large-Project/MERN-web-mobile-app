1fceb03e9b6b5deda5e3a7b444bed90ceef431a0
notrealjohn <johnc33594@gmail.com>
2020-07-22 19:44:29 -0400

    remade basic register page

--- web/src/components/auth/Register.js
+++ web/src/components/auth/Register.js
@@ -1,83 +1,162 @@
-import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
 import axios from 'axios';
 
-const Register = () => {
-    const [formData, setFormData] = useState(
-        {
-            username:'',
-            firstname:'',
-            lastname:'',
-            email:''
+export default class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeUsername = this.onChangeUsername.bind(this)
+        this.onChangeFirstName = this.onChangeFirstName.bind(this)
+        this.onChangeLastName = this.onChangeLastName.bind(this)
+        this.onChangeEmail = this.onChangeEmail.bind(this)
+        this.onChangePassword = this.onChangePassword.bind(this)
+        this.onChangePasswordConfirm = this.onChangePasswordConfirm.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+
+        this.state = {
+            username : '',
+            firstName : '',
+            lastName : '',
+            email : '',
+            password :'',
+            confirmPassword : ''
         }
-    );
-    const{ name, email, password, password2} = formData;
-  
-    const onChange = e => 
-        setFormData({ ...formData, [e.target.name]: e.target.value});
-            
-    const onSubmit = async e => {
-            e.preventDefault();
-            if(password !== password2)
-            {
-                console.log('Password Not Match')
-            }else{
-                console.log('SUCCESS');   
-            }
-        };
-    
-            
-    return <Fragment>
-        
-        <h1 className="large text-primary">Sign Up</h1>
-      <p className="lead"><i class="fas fa-user"></i> Create Your Account</p>
-      <form class="form" onSubmit={e => onSubmit(e)}>
-        <div class="form-group">
-          <input type="text" 
-          placeholder="UserName" 
-          name="name" 
-          value ={name}
-          onChange={e =>onChange(e)}
-          required />
-        </div>
-        <div class="form-group">
-          <input type="email" placeholder="Email Address" name="email" 
-          value ={email}
-          onChange={e =>onChange(e)} required
-          />
-          
-        </div>
-        <div class="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value ={password}
-            onChange={e =>onChange(e)} required
-            minLength="6"
-          />
-        </div>
-        <div class="form-group">
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-            value ={password2}
-          onChange={e =>onChange(e)} required
-            minLength="6"
-          />
-        </div>
-        <input type="submit" class="btn btn-primary" value="SignUp" />
-      </form>
-      <p class="my-1">
-        Already have an account? <Link to="/login">Sign In</Link>
-      </p>
-        
-        </Fragment>;
+    }
+
+    onChangeUsername(e){
+        this.setState({
+            username : e.target.value
+        })
+    }
+     onChangeFirstName(e){
+        this.setState({
+            firstName: e.target.value
+        })
+     }
+
+     onChangeLastName(e){
+        this.setState({
+            lastName : e.target.value
+        })
+     }
+
+     onChangeEmail(e){
+        this.setState({
+            email : e.target.value
+        })
+     }
+
+     onChangePassword(e){
+        this.setState({
+            password:e.target.value
+        })
+     }
+
+     onChangePasswordConfirm(e){
+        this.setState({
+            confirmPassword : e.target.value
+        })
+     }
+
+     onSubmit(e){
+        e.preventDefault()
+
+         if(this.state.password === this.state.confirmPassword)
+         {
+             const user = {
+                 username: this.state.username,
+                 firstName: this.state.firstName,
+                 lastName: this.state.lastName,
+                 email: this.state.email,
+                 password: this.state.password,
+                 confirmPassword: this.state.confirmPassword
+             }
+
+             console.log(user)
+             axios.post('http://localhost:3000/api/auth/register',user)
+                 .then(res => console.log(res.data))
+
+             this.setState({
+                 username : '',
+                 firstName : '',
+                 lastName : '',
+                 email : '',
+                 password :'',
+                 confirmPassword : ''
+             })
+         }
+         else
+         {
+             console.log("Passwords do NOT match")
+         }
 
 
+     }
 
-};
+     render(){
+         return(
+             <div>
+                 <h3>Register A User</h3>
+                 <form onSubmit={this.onSubmit}>
+                     <div className="form-group">
+                         <label>Username: </label>
+                         <input type = "string"
+                                required
+                                className="form-control"
+                                value = {this.state.username}
+                                onChange = {this.onChangeUsername}
+                         />
+                     </div>
+                     <div>
+                         <label>First Name: </label>
+                         <input type = "string"
+                                required
+                                className="form-control"
+                                value = {this.state.firstName}
+                                onChange={this.onChangeFirstName}
+                         />
+                     </div>
+                     <div>
+                         <label>Last Name: </label>
+                         <input type = "string"
+                                required
+                                className="form-control"
+                                value = {this.state.lastName}
+                                onChange={this.onChangeLastName}
+                         />
+                     </div>
+                     <div>
+                         <label>Email: </label>
+                         <input type = "string"
+                                required
+                                className="form-control"
+                                value = {this.state.email}
+                                onChange={this.onChangeEmail}
+                         />
+                     </div>
+                     <div>
+                         <label>Password: </label>
+                         <input type = "string"
+                                required
+                                className="form-control"
+                                value = {this.state.password}
+                                onChange={this.onChangePassword}
+                         />
+                     </div>
+                     <div>
+                         <label>Confirm Password: </label>
+                         <input type = "string"
+                                required
+                                className="form-control"
+                                value = {this.state.confirmPassword}
+                                onChange={this.onChangePasswordConfirm}
+                         />
+                     </div>
+                     <div className="form-group">
+                         <input type ="submit" value = "Register Account" className="btn btn-primary"/>
+                     </div>
+                 </form>
+             </div>
+         )
+     }
+}
 
-
-export default Register
