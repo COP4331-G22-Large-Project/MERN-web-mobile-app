dbdf022d7708db1d9876019c4df511eb4c6e2955
notrealjohn <johnc33594@gmail.com>
2020-07-23 18:23:13 -0400

    added logs and email verification input page

--- web/src/App.js
+++ web/src/App.js
@@ -8,8 +8,9 @@ import Login from './components/auth/Login';
 import LogStool from './components/auth/LogStool';
 import LogExercise from './components/auth/LogExercise';
 import LogFood from './components/auth/LogFood';
-
+import Logs from './components/auth/Logs';
 import './App.css';
+import EmailVerification from "./components/auth/EmailVerification";
 
 
 const App = () => (
@@ -23,6 +24,8 @@ const App = () => (
             <Route exact path="/logfood" component={LogFood}/>
           <Route exact path = "/logstool" component = {LogStool}/>
           <Route exact path="/register" component = {Register} />
+          <Route exact path ="/emailverification" component={EmailVerification}/>
+          <Route exact path ="/logs" component={Logs}/>
           <Route exact path="/login" component = {Login} />
         </Switch>
       </section>
