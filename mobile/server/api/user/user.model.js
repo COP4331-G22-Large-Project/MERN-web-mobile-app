b2b8bb787103758f0934e2280fc85e9b8017e4b4
notrealjohn <johnc33594@gmail.com>
2020-07-25 22:00:15 -0400

    api mixed with mobile_app branch

---
+++ mobile/server/api/user/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+const userSchema = new mongoose.Schema({
+	firstName: String,
+	lastName: String,
+	email: {
+		type: String,
+		unique: true,
+		match: emailRegex
+	},
+	username: {
+		type: String,
+		unique: true
+	},
+	password: String,
+});
+
+// When converting to an object, strip out the password and _id since they are sensitive
+if (!userSchema.options.toObject) userSchema.options.toObject = {};
+userSchema.options.toObject.transform = function (doc, ret, options) {
+	delete ret._id;
+	delete ret.password;
+
+	return ret;
+}
+
+export default (mongoose.model('User', userSchema));
+
+export { emailRegex };
--- mobile/src/RegisterScreen.js
+++ mobile/src/RegisterScreen.js
@@ -1,14 +1,39 @@
 import * as React from "react";
+import axios from 'axios'
 import { Button, TextInput, View, StyleSheet } from "react-native";
 
 export function RegisterScreen() {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
 
   const { container, txtInput } = styles;
 
+  let state;
+  state = {
+    username : '',
+    firstName : '',
+    lastName : '',
+    email : '',
+    password :'',
+    confirmPassword : ''
+  }
+    const user = {
+        username: state.username,
+        firstName: state.firstName,
+        lastName: state.lastName,
+        email: state.email,
+        password: state.password,
+        confirmPassword: state.confirmPassword
+    }
+
   const register = (username, password) => {
-    console.log("You can handle api register here");
+    axios.post('/api/auth/register',user)
+        .then(res => window.location.href = 'Home')
+
   };
 
   return (
@@ -19,13 +44,20 @@ export function RegisterScreen() {
         onChangeText={setUsername}
         style={txtInput}
       />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        style={txtInput}
-      />
+        <TextInput
+            placeholder="firstName"
+            value={firstName}
+            onChangeText={setFirstName}
+            secureTextEntry
+            style={txtInput}
+        />
+        <TextInput
+            placeholder="confirmPassword"
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry
+            style={txtInput}
+        />
       <Button
         title="Register"
         onPress={() => register({ username, password })}
