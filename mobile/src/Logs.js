b7e965b7836ef97ebc8cd170087f91f72b6f3041
checrispUCF <54544426+checrispUCF@users.noreply.github.com>
2020-07-27 19:33:58 -0400

    Pushing Raj's changes

--- mobile/src/Logs.js
+++ mobile/src/Logs.js
@@ -12,69 +12,47 @@ import {
   ScrollView,
 } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-// import { getAllStools} from "../api/stools";
+import { getAllStools } from "../api/stool";
 import Card from "../components/Card";
-import Header from '../components/Header'
-//import { Logs } from "expo";
+import Header from "../components/Header";
 
-// ------ API STUFF -------
-// const Log = (props) => (
-//   <tr>
-//     <td>{props.log.userId}</td>
-//     <td>{props.log.amount}</td>
-//     <td>{props.log.type}</td>
-//     <td>{props.log.date}</td>
-//   </tr>
-// );
 export function Logs() {
   const { container } = styles;
 
   const [consumable, set_consumable] = useState("");
   const [exercise, set_exercise] = useState("");
   const [rating, set_rating] = useState("");
+  const [stools, setStools] = useState([]);
 
-  // ------ API STUFF -------
-  // const logList = () => {
-  // return this.state.stools.map((currentLog) => {
-  //   return (
-  //     <Log log={currentLog} deleteLog={this.deleteLog} key={currentLog._id} />
-  //     );
-  //   });
-  // };
+  useEffect(() => {
+    const bootstrapAsync = () => {
+      getAllStools()
+        .then((res) => {
+          this.setState({ stools: res.data });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    bootstrapAsync();
+  }, []);
 
+  const renderItem = (data) => {
+    return (
+      <View>
+        <Text>{data.item.name}</Text>
+      </View>
+    );
+  };
   const pressHandler = () => {
     alert("Thank you for submitting!");
     // this.logList();
   };
 
-  // React.useEffect(() => {
-  //   // Fetch the token from storage then navigate to our appropriate place
-  //   const bootstrapAsync = () => {
-  //     getAllStools()
-  //       .then((res) => {
-  //         console.log('restore success');
-  //         console.log(res.headers);
-  //         dispatch({ type: "RESTORE_TOKEN", user: res.data });
-  //       })
-  //       .catch((err) => {
-  //         console.log('restore fail');
-  //         console.log(err.response.headers);
-  //         dispatch({ type: "RESTORE_TOKEN" });
-  //       })
-  //   };
-
-    const stoolDisplay = () => {
-      return this.getAllStools.map ((currentLog) => {
-        return (
-          <Log log = {currentLog} key = {currentLog._id} />
-        );
-      });
-    };
-
   return (
     <ScrollView contentContainerStyle={container}>
-      <Header title= 'Your Personal Logs'/>
-      <Card style = {styles.CardContainer}>
+      <Header title="Your Personal Logs" />
+      <Card style={styles.CardContainer}>
         <Text>Filter based on rating</Text>
         <Text>Bristol Rating:</Text>
         <TextInput
@@ -85,10 +63,13 @@ export function Logs() {
         <Button title="Search" onPress={pressHandler} />
       </Card>
 
-      <Card>
-   
+      <Card style={styles.CardContainer}>
+        <FlatList
+          style={styles.flatListView}
+          data={stools}
+          renderItem={renderItem}
+        />
       </Card>
-      
     </ScrollView>
   );
 }
@@ -97,10 +78,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-  
   },
-  CardContainer:{
-    alignItems: 'center',
+  CardContainer: {
+    alignItems: "center",
     width: 330,
     marginVertical: 30,
   },
@@ -110,5 +90,9 @@ const styles = StyleSheet.create({
     padding: 8,
     margin: 10,
     width: 200,
+  },
+  flatListView: {
+    width: "100%",
+    height: 40,
   },
 });
