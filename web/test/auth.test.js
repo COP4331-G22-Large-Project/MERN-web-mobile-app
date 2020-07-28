e7289fc1cd3290acd2dd0abcd243a66fbd238177
Noah Gil <xlash19o13@gmail.com>
2020-07-27 22:29:42 -0400

    Upgraded Babel

--- web/test/auth.test.js
+++ web/test/auth.test.js
@@ -9,5 +9,5 @@ test('generates password with salt', () => {
 	const fakeUser = { _id: '12345' };
 	const password = 'password';
 	const result = '712af191df3a552905140a07a6d8f0ee0e18e4514815ef2cad51c386a747dbff56f0a2b659391f53d406eb5b7e8569b1bb01e1a2f5f581ab5717db9989d534de';
-	expect(generatePasswordWithSalt(fakeUser, result)).toBe(result);
+	expect(generatePasswordWithSalt(fakeUser, password)).toBe(result);
 });
