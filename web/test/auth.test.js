4d79590ddf9cf84cb1fbe58fc0814ca7d3301ac3
DrPeeper <coydiego@knights.ucf.edu>
2020-07-27 22:11:04 -0500

    isDuplicate unit test skeleton

--- web/test/auth.test.js
+++ web/test/auth.test.js
@@ -11,3 +11,14 @@ test('generates password with salt', () => {
 	const result = '712af191df3a552905140a07a6d8f0ee0e18e4514815ef2cad51c386a747dbff56f0a2b659391f53d406eb5b7e8569b1bb01e1a2f5f581ab5717db9989d534de';
 	expect(generatePasswordWithSalt(fakeUser, result)).toBe(result);
 });
+
+test('checks db for given user and email', () => {
+	const user = "dummy1";
+	const email = "dummy1@gmail.com";"
+
+	expect(isUserDuplicate(user, email)).objectContaining(
+		{
+			isUsername: true,
+			isEmail: true
+		});
+});
