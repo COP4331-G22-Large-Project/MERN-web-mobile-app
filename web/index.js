9e83c2c5690e26d25126001bc296ca2727eeebe9
notrealjohn <johnc33594@gmail.com>
2020-07-28 09:56:00 -0400

    signin works now, logs doesnt, attempting fix logs

--- web/index.js
+++ web/index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 else {
-	app.use(express.static(path.join(__dirname, '/client/public')));
+	app.use(express.static(path.join(__dirname, '/public')));
 	app.get("/*", function(req, res) {
 		res.sendFile(path.join(__dirname, "./public/index.html"));
 	});
